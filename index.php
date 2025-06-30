c6d0065cbefe4818f4ffd6e0fb51e80398a54c9c
rjbelleza <rjbelleza19@gmail.com>
2025-06-30 20:44:52 +0800

    Added header

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <title>Nauto | Roadmap to Improving Fleet Safety</title>
+</head>
+<body>
+    <?php include "components/header.html" ?>
+</body>
+</html>
